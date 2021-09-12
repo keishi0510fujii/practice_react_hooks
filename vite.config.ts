2fc3f75ee3c08c4734c46e8b64abb89fafce00fa
keishi0510fujii <keishifujii0510@gmail.com>
2021-09-12 18:06:44 +0900

    create template

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 7002,
+  },
   plugins: [reactRefresh()]
 })
