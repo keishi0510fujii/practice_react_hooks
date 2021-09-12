426717e6487b321b29a493bebc1a0ad2a67d4d63
keishi0510fujii <keishifujii0510@gmail.com>
2021-09-12 19:09:27 +0900

    implement component using multiple states

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,45 @@
 // eslint-disable-next-line no-use-before-define
 import React, { useState } from 'react';
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-const App = () => {
-  const [count, setCount] = useState(0);
+type DefaultProps = {
+    name?: string;
+    price?: number;
+};
 
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
-  const increment2 = () => setCount((previousCount: number) => previousCount + 1);
-  const decrement2 = () => setCount((previousCount: number) => previousCount - 1);
-  const resetCount = () => setCount(0);
-  const double = () => setCount((previousCount: number) => previousCount * 2);
-  // eslint-disable-next-line max-len
-  const divideThree = () => setCount(
-    (prevCount: number) => (prevCount % 3 === 0 ? prevCount / 3 : prevCount),
-  );
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+const App = (props: DefaultProps) => {
+  // eslint-disable-next-line react/jsx-filename-extension
+
+  // eslint-disable-next-line react/destructuring-assignment
+  const [name, setName] = useState(props.name);
+  // eslint-disable-next-line react/destructuring-assignment
+  const [price, setPrice] = useState(props.price);
+
+  const reset = () => {
+    setPrice(props.price);
+    setName(props.name);
+  };
+
   return (
   // eslint-disable-next-line react/jsx-filename-extension
     <>
-      <div>
-        count:
-        {' '}
-        {count}
-      </div>
-      <div>
-        <button type="button" onClick={increment}>+1</button>
-        <button type="button" onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button type="button" onClick={increment2}>+1</button>
-        <button type="button" onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button type="button" onClick={resetCount}>Reset</button>
-        <button type="button" onClick={double}>x2</button>
-        <button type="button" onClick={divideThree}>3の倍数の時だけ3で割る</button>
-      </div>
+      <p>
+        現在の
+        {name}
+        は、
+        {price}
+        です
+      </p>
+      <button type="button" onClick={() => setPrice(price + 1)}>+1</button>
+      <button type="button" onClick={() => setPrice(price - 1)}>-1</button>
+      <button type="button" onClick={reset}>Reset</button>
+      <input value={name} type="text" onChange={(e) => setName(e.target.value)} />
     </>
   );
 };
 
+App.defaultProps = {
+  name: 'サンプル',
+  price: 1000,
+};
 export default App;
