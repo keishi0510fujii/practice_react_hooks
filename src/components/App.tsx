cd686fefd4425a6299e0ca5f135c0a6746481baf
keishi0510fujii <keishifujii0510@gmail.com>
2021-09-14 06:53:55 +0900

    setup react context provider

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,16 +4,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import reducer from '../reducers/index';
 import EventForm from './EventForm';
 import Events from './Events';
+import AppContext from '../contexts/AppContext';
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const App = () => {
   // eslint-disable-next-line react/jsx-filename-extension
   const [state, dispatch] = useReducer(reducer, []);
 
   return (
-    <div className="container-fluid">
-      <EventForm state={state} dispatch={dispatch} />
-      <Events state={state} dispatch={dispatch} />
-    </div>
+    <AppContext.Provider value="hello provider">
+      <div className="container-fluid">
+        <EventForm state={state} dispatch={dispatch} />
+        <Events state={state} dispatch={dispatch} />
+      </div>
+    </AppContext.Provider>
   );
 };
 
--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Event from './Event';
+import AppContext from '../contexts/AppContext';
 
-const Events = ({ state, dispatch }) => (
-  <>
-    <h4>イベント一覧</h4>
-    <table className="table table-hover">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>タイトル</th>
-          <th>ボディー</th>
-          <th />
-        </tr>
-      </thead>
-      <tbody>
-        {state.map((event: any, index: number) => (
-          <Event key={index} event={event} dispatch={dispatch} />
-        )) }
-      </tbody>
-    </table>
-  </>
-);
+const Events = ({ state, dispatch }) => {
+  const value = useContext(AppContext);
+  return (
+    <>
+      <div>{value}</div>
+      <h4>イベント一覧</h4>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>タイトル</th>
+            <th>ボディー</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          {state.map((event: any, index: number) => (
+            <Event key={index} event={event} dispatch={dispatch} />
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
 
 export default Events;
